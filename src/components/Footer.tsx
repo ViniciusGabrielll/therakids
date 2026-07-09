42fb2b496ddf7c93815c6b2d68c9812595c2292d
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-07-09 15:14:17 -0300

    feat(welcome): add welcome css and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import styles from "./Footer.module.css";
+import logo from "../../public/logos/whiteLogo.svg"
+
+import footerDecoration from "../../public/decorations/footer.svg"
+
+import type { ReactNode } from "react";
+
+interface FooterProps {
+    children: ReactNode;
+}
+
+export default function Footer({ children }: FooterProps) {
+    return (
+        <footer className={styles.footer}>
+            <img src={footerDecoration} alt="Footer" className={styles.footerDecoration} />
+            <div className={styles.container}>
+                <section className={styles.content}>
+                    <div className={styles.brand}>
+                        <img src={logo} alt="Logo" />
+
+                        <p>
+                            Apoiamos o desenvolvimento infantil
+                            de forma integral, respeitosa e
+                            individualizada.
+                        </p>
+                    </div>
+                </section>
+
+                {children}
+            </div>
+
+            <span className={styles.copy}>
+                © 2026 TheraKids. Todos os direitos reservados.
+            </span>
+        </footer>
+    );
+}
--- src/containers/Welcome.tsx
+++ src/containers/Welcome.tsx
@@ -1,11 +1,138 @@
-import Header from "../components/header";
+import styles from "./Welcome.module.css";
+
+import logo from "../../public/logos/logo.svg"
+
+import redWhatsapp from "../../public/icons/red/whatsapp.svg"
+import redInstagram from "../../public/icons/red/instagram.svg"
+import redMaps from "../../public/Icons/red/maps.svg"
+import greenWhatsapp from "../../public/icons/green/whatsapp.svg"
+import greenInstagram from "../../public/icons/green/instagram.svg"
+import greenMaps from "../../public/Icons/green/maps.svg"
+
+import clinic from "../../public/Icons/clinic.svg"
+import Footer from "../components/Footer";
 
 export default function Welcome() {
     return (
-        <div>
-            <Header/>
-            <h1>Bem Vindo</h1>
-            <p>Conteúdo da página Bem Vindo</p>
-        </div>
+        <>
+            <article className={styles.welcome}>
+                <section className={styles.hero}>
+                    <img src={logo} alt="Logo" className={styles.logo} />
+                    <h1 className={styles.title}>Bem vindo(a) à <span style={{ color: "var(--primary-color)" }}>TheraKids</span>!</h1>
+                    <p className={styles.description}>Para proporcionar uma experiência personalizada, escolha a unidade que deseja acessar.
+                        Assim você visualizará os Serviços, contatos e informações correspondente à unidade selecionada.</p>
+                    <p className={styles.description}>Você poderá alterar essa escolha sempre que desejar.</p>
+                </section>
+
+                <h2 className={styles.secondTitle}>Escolha sua <span style={{ color: "var(--primary-color)" }}>unidade</span></h2>
+                <section className={styles.cardSection}>
+
+                    <div className={styles.card}>
+                        <div style={{ backgroundImage: "url(../../public/unidadeFlores.svg)" }} className={styles.cardImage} />
+                        <div className={styles.cardContent}>
+                            <h2 style={{ color: "var(--primary-color)" }}>Unidade Flores</h2>
+                            <div className={styles.localization}>
+                                <img src={redMaps} alt="Localização" />
+                                <p>Rua Barão de Surui, 313,Flores, Manaus/Am</p>
+                            </div>
+                            <div className={styles.contactInfo} style={{ backgroundColor: "rgba(255, 152, 148, 20%)" }}>
+                                <a>
+                                    <img src={redWhatsapp} alt="Número" />
+                                    <h3>(92) 9 9333-5257</h3>
+                                </a>
+                                <a href="https://www.instagram.com/therakids_unidade_flores/" target="_blank">
+                                    <img src={redInstagram} alt="Instagram" />
+                                    <h3>@therakids_unidade_flores</h3>
+                                </a>
+                            </div>
+                            <a href="/unidades/flores" className={styles.cardLink} style={{ backgroundColor: "var(--primary-color)" }}>
+                                <p>Acessar Unidade Flores</p>
+                                <div style={{ backgroundColor: "color-mix(in srgb, var(--primary-color) 80%, white)" }}>
+                                    <p>{">"}</p>
+                                </div>
+                            </a>
+
+                            <div className={styles.clinic} style={{ backgroundColor: "var(--primary-color)" }}>
+                                <img src={clinic} alt="clínica" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.card}>
+                        <div style={{ backgroundImage: "url(../../public/unidadeDomPedro1.svg)" }} className={styles.cardImage} />
+                        <div className={styles.cardContent}>
+                            <h2 style={{ color: "var(--additional-color)" }}>Unidade Dom Pedro I</h2>
+                            <div className={styles.localization}>
+                                <img src={greenMaps} alt="Localização" />
+                                <p>Rua Barão de Surui, 313,Flores, Manaus/Am</p>
+                            </div>
+                            <div className={styles.contactInfo} style={{ backgroundColor: "rgba(190, 225, 115, 20%)" }}>
+                                <a>
+                                    <img src={greenWhatsapp} alt="Número" />
+                                    <h3>(92) 9 9313-0472</h3>
+                                </a>
+                                <a href="https://www.instagram.com/therakids_unidade_dom_pedro1/" target="_blank">
+                                    <img src={greenInstagram} alt="Instagram" />
+                                    <h3>@therakids_unidade_dom_pedro1</h3>
+                                </a>
+                            </div>
+                            <a href="/unidades/dom-pedro-i" className={styles.cardLink} style={{ backgroundColor: "var(--additional-color)" }}>
+                                <p>Acessar Unidade Dom Pedro I</p>
+                                <div style={{ backgroundColor: "color-mix(in srgb, var(--additional-color) 80%, white)" }}>
+                                    <p>{">"}</p>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div className={styles.clinic} style={{ backgroundColor: "var(--additional-color)" }}>
+                            <img src={clinic} alt="clínica" />
+                        </div>
+                    </div>
+                </section>
+            </article>
+
+            <Footer>
+                <section>
+                    <h4>Navegação</h4>
+
+                    <a href="/unidades/flores">Unidade Flores</a>
+                    <a href="/unidades/dom-pedro-i">Unidade Dom Pedro I</a>
+                </section>
+
+                <section>
+                    <h4>Contato</h4>
+
+                    <div>
+                        <h5>Unidade Flores</h5>
+                        <p>+55 92 9333-5257</p>
+                    </div>
+
+                    <div>
+                        <h5>Unidade Dom Pedro I</h5>
+                        <p>+55 92 9313-0472</p>
+                    </div>
+                </section>
+
+                <section>
+                    <h4>Redes Sociais</h4>
+
+                    <a
+                        href="https://www.instagram.com/therakids_unidade_flores/"
+                        target="_blank"
+                    >
+                        @therakids_unidade_flores
+                    </a>
+
+                    <a
+                        href="https://www.instagram.com/therakids_unidade_dom_pedro1/"
+                        target="_blank"
+                    >
+                        @therakids_unidade_dom_pedro1
+                    </a>
+                </section>
+
+                <section></section>
+            </Footer>
+        </>
     );
 }
